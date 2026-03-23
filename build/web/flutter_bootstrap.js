c0b879a36ebd910e7a7520e2317401d3050666fa
Nithish-Krishna <nithish.krishna0101@gmail.com>
2026-03-23 14:15:54 +0530

    fix: updated dependencies.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2665894373" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1711849467" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
